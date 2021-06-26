a6f33ff37e1958cdbe4d83195260be122a666820
halhal <hh.25799623@gmail.com>
2021-06-26 23:04:15 +0900

    add graphql server

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module microtips
 go 1.16
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/mattn/go-sqlite3 v1.14.7 // indirect
+	github.com/99designs/gqlgen v0.13.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.7
+	github.com/vektah/gqlparser/v2 v2.1.0 // indirect
 	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
 	google.golang.org/genproto v0.0.0-20210624195500-8bfb893ecb84 // indirect
-	google.golang.org/grpc v1.38.0 // indirect
-	google.golang.org/protobuf v1.27.0 // indirect
+	google.golang.org/grpc v1.38.0
+	google.golang.org/protobuf v1.27.0
 )
---
+++ graph/server/server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"microtips/article/client"
+	"microtips/graph"
+	"microtips/graph/generated"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	// articleClientを生成
+	articleClient, err := client.NewClient("localhost:50051")
+	if err != nil {
+		articleClient.Close()
+		log.Fatalf("Failed to create article client: %v\n", err)
+	}
+
+	// GraphQLサーバーに先程のResolverを実装
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{ArticleClient: articleClient}}))
+
+	// GraphQL playgroundのエンドポイント
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	//　実装したクエリが実行可能なGraphQLサーバーのエンドポイント
+	http.Handle("/query", srv)
+
+	// GraphQLサーバーを起動
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
