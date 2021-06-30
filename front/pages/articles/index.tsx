7e2c52cd1a428521339dbfe458389c8928d57b5a
halhal <hh.25799623@gmail.com>
2021-06-30 20:47:20 +0900

    add articles pages for front

---
+++ front/pages/articles/index.tsx
@@ -0,0 +1,22 @@
+import {useQuery} from '@apollo/client'
+import { NextPage } from 'next'
+import { ArticlesData, ARTICLES_QUERY } from '../../graphql/queries/articles.query'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface IndexProps {}
+
+const Index: NextPage<IndexProps> = () => {
+  // const { data } = useQuery<ArticlesData>(ARTICLES_QUERY);
+  // const { articles } = data
+  // if (!articles) return <p>nothing to data</p> 
+  return (
+    <>
+      <h2>hello world list</h2>
+      {
+        // articles.map(article => <p key={article.id}>{article.title}</p>)
+      }
+    </>
+  )
+}
+
+export default Index
